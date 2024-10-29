87594147e8ae7b865dd982d67c9bab0034b40ff5
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-10-29 21:17:35 +0200

    feat: Implement endpoint for getting user commits from a particular repository

--- src/routes/contributionsRouter.ts
+++ src/routes/contributionsRouter.ts
@@ -3,13 +3,16 @@ import { print } from "graphql";
 
 import { ResultOf, VariablesOf } from "@graphql-typed-document-node/core";
 import {
+    CommitsDocument,
     IssueCommentsDocument,
     IssuesDocument,
     PullRequestReviewsDocument,
     PullRequestsDocument,
     UserCommitCommentsDocument,
+    UserInfoDocument,
 } from "../graphql/typed_queries.js";
 import { DateWindows } from "../utils/DateWindows.js";
+import NotGithubUser from "../utils/errors/NotGithubUser.js";
 import { getQueryNodes } from "./helpers/getQueryNodes.js";
 import { sendQueryWindowedPaginated } from "./helpers/sendQueries.js";
 import { streamResponse } from "./helpers/sendStreamChunk.js";
@@ -19,13 +22,47 @@ export { router as contributionsRouter };
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/commits/:githubId/:owner/:name/:fromDate/:toDate", async (req, res) => {
+// Get commits from repository
+router.get("/commits/:owner/:name/:fromDate/:toDate", async (req, res) => {
     const { octokit } = req;
-    const { login, fromDate, toDate, githubId, owner, name } = req.params as typeof req.params & {
+    const { login, owner, name } = req.params as typeof req.params & {
         login: string;
     };
+    const fromDate = new Date(req.params.fromDate);
+    const toDate = new Date(req.params.toDate);
 
-    res.status(501).end(JSON.stringify({ message: "Not Implemented" }));
+    const userInfoVariables: VariablesOf<typeof UserInfoDocument> = {
+        login,
+    };
+    const userInfo = await octokit.graphql<ResultOf<typeof UserInfoDocument>>(
+        print(UserInfoDocument),
+        userInfoVariables
+    );
+
+    if (!userInfo.user) {
+        throw new NotGithubUser(login);
+    }
+
+    const commitsQueryVariables: VariablesOf<typeof CommitsDocument> = {
+        owner,
+        name,
+        authorId: userInfo.user.id,
+        fromDate,
+        toDate,
+        cursor: null,
+    };
+
+    const commitsQueryFn = sendQueryWindowedPaginated(
+        octokit,
+        CommitsDocument,
+        commitsQueryVariables
+    );
+    const commitsQueryResult = await commitsQueryFn([fromDate, toDate]);
+    const commits = await getQueryNodes(commitsQueryResult);
+
+    await Promise.all(commits.map(streamResponse(res)));
+
+    res.end();
 });
 
 router.get("/issues/from/:fromDate/to/:toDate", async (req, res) => {
