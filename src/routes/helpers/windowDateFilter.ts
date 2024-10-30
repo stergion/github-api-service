d58091a3b7e09f707735eb077a6657bbbf08cf63
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-10-31 00:33:21 +0200

    fix: Write better types and handle null vallues

--- src/routes/helpers/windowDateFilter.ts
+++ src/routes/helpers/windowDateFilter.ts
@@ -1,5 +1,6 @@
 export function windowDateFilter(onProperty: string, fromDate: Date, toDate: Date) {
-    return (item: { [key: string]: any; }) => {
+    return (item: Record<string, any>|null) => {
+        if (!item) return false;
         const value = new Date(item[onProperty]);
         return fromDate <= value && value <= toDate;
     };
