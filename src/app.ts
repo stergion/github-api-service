b4f8a816eaa472f39a6b68de80f1e1d565170f0d
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-11-04 01:47:39 +0200

    feat: Enable swagger for API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 
 import express from "express";
+import swaggerJsdoc, { OAS3Options } from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 import { injectOctokit } from "./middleware/octokit.js";
 import { RepositoryRouter } from "./routes/RepositoryRouter.js";
@@ -10,10 +12,30 @@ import { UserRouter } from "./routes/UserRoutes.js";
 
 const port = process.env["PORT"] ?? 5000;
 
+const options: OAS3Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "GitHub Client Service API",
+            version: "1.0.0",
+            description: "API documentation for GitHub Client Service",
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`, 
+            },
+        ],
+    },
+    apis: ["./dist/routes/*.js", "./swagger/schemas/*.yml", "./swagger/examples/*.yml"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
 const app = express();
 
 // Middleware
 app.use(injectOctokit());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routers
 app.use("/api/user", UserRouter);
