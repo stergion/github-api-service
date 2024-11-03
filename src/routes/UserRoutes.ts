17faff0d7096b1e6ab6352d27aa16e108902f318
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-11-04 01:40:33 +0200

    feat: Add Swagger documentation for user information endpoint

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -6,6 +6,25 @@ export { router as UserRouter };
 
 const router = express.Router({ mergeParams: true });
 
+/**
+ * @swagger
+ * /api/user/{login}:
+ *   get:
+ *     summary: Get user information
+ *     parameters:
+ *       - name: login
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/UserInfo'
+ */
 router.get("/:login", async (req, res) => {
     const { octokit, params: { login } } = req;
 
