533a1317bf0e579e47f6a4d1d74a25088727ac4e
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2025-01-07 16:20:32 +0200

    feat: implement heartbeat mechanism in SSEStream

--- src/utils/SSEStream.ts
+++ src/utils/SSEStream.ts
@@ -1,22 +1,37 @@
 import { Response } from "express";
 import { SSEError } from "./errors/SSEError.js";
 
+const HEARTBEAT_INTERVAL_MS = process.env["HEARTBEAT_INTERVAL_MS"]
+    ? parseInt(process.env["HEARTBEAT_INTERVAL_MS"])
+    : 30_000;
+
 export class SSEStream {
     private res: Response;
+    private interval!: NodeJS.Timeout;
 
-    constructor(res: Response) {
+    constructor(res: Response);
+    constructor(res: Response, intervalMs: number);
+    constructor(res: Response, intervalMs?: number) {
+        const heartbeatInterval = intervalMs ?? HEARTBEAT_INTERVAL_MS;
+
         this.res = res;
         if (!res.headersSent) {
             this.initializeHeaders();
         }
+        
+        this.interval = this.startHeartbeat(heartbeatInterval);
+        res.on("close", () => {
+            console.log("close event fired!!!");
+            this.stopHeartbeat();
+        });
     }
 
     private initializeHeaders(): void {
-        this.res.setHeader("X-SSE-Content-Type", "application/json")
+        this.res.setHeader("X-SSE-Content-Type", "application/json");
         this.res.setHeader("Content-Type", "text/event-stream");
         this.res.setHeader("Cache-Control", "no-cache");
         this.res.setHeader("Connection", "keep-open");
-        this.res.status(200)
+        this.res.status(200);
 
         // Flush headers immediately
         if ("flushHeaders" in this.res) {
@@ -48,5 +63,19 @@ export class SSEStream {
 
     public streamError(error: SSEError) {
         this.writeError(error.toData());
+        this.stopHeartbeat();
+    }
+
+    private sendHeartbeat(): void {
+        this.res.write("event: heartbeat\n\n");
+    }
+
+    private startHeartbeat(intervalMs: number = 30000) {
+        this.sendHeartbeat();
+        return setInterval(() => this.sendHeartbeat(), intervalMs);
+    }
+
+    public stopHeartbeat(): void {
+        clearInterval(this.interval);
     }
 }
